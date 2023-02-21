9ced33b4b9197d52f7b58c5a9f4d5e584d4bc0f2
syrizelink <2532124404@qq.com>
2023-02-21 18:29:51 +0800

    样式改进

--- generate.js
+++ generate.js
@@ -1,11 +1,17 @@
+let x = document.getElementById("copyComplete");
+
 function AncientCode() {
     let unicode = Math.floor(Math.random() * (0x9fbf - 0x3400 + 1)) + 0x3400;
     document.getElementById("textArea").value = String.fromCharCode(unicode);
+    document.getElementById("copy").disabled = "";
+    x.className = "copyComplete";
 }
 
 function copy() {
     let text = document.getElementById("textArea");
     text.select();
-    document.execCommand("Copy");
-    alert("复制成功")
+    if (document.execCommand("Copy")){
+        document.execCommand("Copy");
+    }
+    x.className = 'copyCompleteIn';
 }
